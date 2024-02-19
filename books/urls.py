ed4836b1aa5b3110dd1c80f446504faef90289b9
nsdNite <nsd7night@gmail.com>
2024-02-19 12:55:15 +0200

    Add urls for book CRUD

---
+++ books/urls.py
@@ -0,0 +1,10 @@
+from rest_framework.routers import DefaultRouter
+
+from books.views import BookViewSet
+
+router = DefaultRouter()
+router.register(r"books", BookViewSet)
+
+urlpatterns = router.urls
+
+app_name = "books"
