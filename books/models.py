1d8147dfb23fc6d14f86910b909853185d945e31
nsdNite <nsd7night@gmail.com>
2024-02-19 12:36:17 +0200

    Add models for Book
    Add custom cover enum class for option select

--- books/models.py
+++ books/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from enum import Enum
+
+
+class CoverType(Enum):
+    HARD = "Hard cover"
+    SOFT = "Soft cover"
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=125)
+    cover = models.CharField(
+        max_length=10, choices=[(tag, tag.value) for tag in CoverType]
+    )
+    inventory = models.PositiveIntegerField(default=0)
+    daily_fee = models.DecimalField(max_digits=2, decimal_places=2)
