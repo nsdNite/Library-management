7406fd4f57ad4fe6aa8fa7bbb8482eb07b67266f
nsdNite <nsd7night@gmail.com>
2024-02-19 12:52:17 +0200

    Add basic views for Book model

--- books/views.py
+++ books/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from books.models import Book
+from books.serializers import (
+    BookSerializer,
+    BookListSerializer,
+    BookDetailSerializer,
+)
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    # permission_classes = [IsAuthenticated]
+    # pagination_class = BookPagination
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return BookListSerializer
+
+        if self.action == "retrieve":
+            return BookDetailSerializer
+
+        return BookSerializer
