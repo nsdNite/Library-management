95af3aba6e0594ef108be451cb64f0a4c5a54cf7
nsdNite <nsd7night@gmail.com>
2024-02-19 13:56:06 +0200

    Add user app
    Implemented registration with email
    Update core urls and setting accordingly

--- library_management/settings.py
+++ library_management/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "books",
+    "user",
 ]
 
 MIDDLEWARE = [
@@ -103,6 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "user.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
