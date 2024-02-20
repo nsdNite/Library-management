50acb6902b575e8dd0ebb951d101f4f636a2acf5
nsdNite <nsd7night@gmail.com>
2024-02-20 21:56:43 +0200

    Add custom action for getting borrowing by user id and status

--- books/views.py
+++ books/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from books.models import Book, Borrowing
 from books.serializers import (
@@ -27,3 +29,19 @@ class BookViewSet(viewsets.ModelViewSet):
 class BorrowingViewSet(viewsets.ModelViewSet):
     queryset = Borrowing.objects.all()
     serializer_class = BorrowingSerializer
+
+    @action(detail=False, methods=["get"])
+    def get_borrowing_by_user_and_status(self, request):
+        user_id = request.query_params.get("user_id")
+        is_active = request.query_params.get("is_active") == "true"
+
+        queryset = self.get_queryset().filter(user_id=user_id)
+
+        if is_active:
+            queryset = queryset.filter(actual_return_date__isnull=True)
+        else:
+            queryset = queryset.exclude(actual_return_date__isnull=True)
+
+        serializer = self.get_serializer(queryset, many=True)
+
+        return Response(serializer.data)
