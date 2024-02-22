c8ca271116424dbc915365f1025130ec4f14462f
nsdNite <nsd7night@gmail.com>
2024-02-22 08:57:00 +0200

    Update logic of validator in serializer
    Now serializer correctly validates data, sets current user and lowering book inventory num by 1.

--- borrowing_service/serializers.py
+++ borrowing_service/serializers.py
@@ -3,6 +3,8 @@ from rest_framework import serializers
 from books.serializers import BookDetailSerializer
 from borrowing_service.models import Borrowing
 
+from datetime import datetime
+
 
 class BorrowingSerializer(serializers.ModelSerializer):
     class Meta:
@@ -17,7 +19,7 @@ class BorrowingSerializer(serializers.ModelSerializer):
         )
 
 
-class BorrowingDetailSerializer(BorrowingSerializer):
+class BorrowingDetailSerializer(serializers.ModelSerializer):
     book = BookDetailSerializer(read_only=True)
 
     class Meta:
@@ -32,7 +34,7 @@ class BorrowingDetailSerializer(BorrowingSerializer):
         )
 
 
-class BorrowingListSerializer(BorrowingSerializer):
+class BorrowingListSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Borrowing
@@ -40,6 +42,38 @@ class BorrowingListSerializer(BorrowingSerializer):
             "id",
             "borrow_date",
             "expected_return_date",
-            "actual_return_date",
             "user",
         )
+
+
+class BorrowingCreateSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    class Meta:
+        model = Borrowing
+        fields = ("id", "expected_return_date", "book", "user")
+
+    def validate(self, data):
+        """Validate next items:
+        - expected return date is after borrowing date;
+        - book is not out of stock."""
+        current_date = datetime.now()
+        borrow_date = datetime.date(current_date)
+        if data["expected_return_date"] <= borrow_date:
+            raise serializers.ValidationError(
+                "Expected return date must be after borrow date."
+            )
+
+        book = data.get("book")
+        if book.inventory == 0:
+            raise serializers.ValidationError("Book is out of stock.")
+
+        return data
+
+    def create(self, validated_data):
+        """Create a new borrowing, lowering book's inventory by 1."""
+        book = validated_data.get("book")
+        book.inventory -= 1
+        book.save()
+
+        return Borrowing.objects.create(**validated_data)
