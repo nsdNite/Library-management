de68e74478edbe8d80a83efbb7e0c9327e123fae
nsdNite <nsd7night@gmail.com>
2024-02-24 10:28:25 +0200

    Add filtering for borrowings by id and status
    Update view with get_queryset for both parameters
    Test in postman -> OK

--- borrowing_service/views.py
+++ borrowing_service/views.py
@@ -20,6 +20,23 @@ class BorrowingViewSet(viewsets.ModelViewSet):
     queryset = Borrowing.objects.all()
     serializer_class = BorrowingSerializer
 
+    def get_queryset(self):
+        """Filter borrowings by user and by status(active)
+        endpoint:
+        GET /api/borrowings/?user_id=123&is_active=true
+        """
+        queryset = Borrowing.objects.all()
+        user_id = self.request.query_params.get("user_id", None)
+        is_active = self.request.query_params.get("is_active", None)
+
+        if user_id:
+            queryset = queryset.filter(user_id=user_id)
+
+        if is_active:
+            queryset = queryset.filter(actual_return_date__isnull=True)
+
+        return queryset.distinct()
+
     def get_serializer_class(self):
         if self.action == "list":
             return BorrowingListSerializer
