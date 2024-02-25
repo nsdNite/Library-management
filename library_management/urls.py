f41fec22c71af47dea8a3b84a6b89d9c74818970
nsdNite <nsd7night@gmail.com>
2024-02-25 13:48:26 +0200

    Update settings for swagger support

--- library_management/urls.py
+++ library_management/urls.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from django.urls import include, path
+
 from rest_framework.routers import DefaultRouter
+
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
 
 from books.urls import router as book_router
 from borrowing_service.urls import router as borrowing_router
@@ -13,4 +20,15 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(router_main.urls)),
     path("api/user/", include("user.urls", namespace="user")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/schema/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/schema/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
 ]
